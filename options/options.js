fb9348ab5ec165c5689335b3744e5db9ca52d5a9
Crowdbotics <team@crowdbotics.com>
2023-10-12 00:45:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-moon-43888",
+  url: "https://mute-moon-43888.botics.co",
+  api: "https://mute-moon-43888.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
